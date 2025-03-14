1ac49381be03428652366b0e86396cad4b80c2f0
Cyril <cyril.beaufrere@gmail.com>
2025-03-14 10:25:38 +0100

    Quartz sync: Mar 14, 2025, 10:25 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "MrXcrypt",
+    pageTitle: "MrDario",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
