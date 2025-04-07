eb0de2a3f47035ec19ef6056ecadbacdd42fa6dd
Cyril <cyril.beaufrere@gmail.com>
2025-04-07 19:32:25 +0200

    Quartz sync: Apr 7, 2025, 7:32 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -63,9 +63,6 @@ const config: QuartzConfig = {
   plugins: {
     transformers: [
       Plugin.FrontMatter(),
-      Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
-      }),
       Plugin.SyntaxHighlighting({
         theme: {
           light: "github-light",
